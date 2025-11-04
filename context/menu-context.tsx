4ee7ec7a0fcb8f9a58d015ee31adf1b7c2fb6466
Muhammad Iqbal <muhammadiqbalid43@gmail.com>
2025-11-04 07:35:08 +0700

    feat: add menu context

---
+++ context/menu-context.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { createContext, ReactNode, useContext, useState } from "react";
+
+interface MenuContextType {
+  activeMenu: string;
+  setActiveMenu: (menu: string) => void;
+}
+
+const MenuContext = createContext<MenuContextType | undefined>(undefined);
+
+export function MenuProvider({ children }: { children: ReactNode }) {
+  const [activeMenu, setActiveMenu] = useState("/dashboard/home");
+
+  return (
+    <MenuContext.Provider value={{ activeMenu, setActiveMenu }}>
+      {children}
+    </MenuContext.Provider>
+  );
+}
+
+export function useMenu() {
+  const context = useContext(MenuContext);
+  if (!context) {
+    throw new Error("useMenu must be used within MenuProvider");
+  }
+  return context;
+}
